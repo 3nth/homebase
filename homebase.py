81096398386855e30af20c3da726375e974a6c55
Derek Flenniken <EMAIL>
2013-05-25 16:30:40 -0700

    powerline config

--- homebase.py
+++ homebase.py
@@ -12,14 +12,17 @@ parser.read('homebase.conf')
 # check if gcc is available
 
 # install brew
-#subprocess.call('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"', shell=True)
+if subprocess.check_output('type -P brew', shell=True).strip() == "":
+	subprocess.call('true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"', shell=True)
 
 # update brew
 subprocess.call('brew update', shell=True)
 
+# install brews
 kegs_installed = subprocess.check_output('brew list', shell=True).split()
 
 kegs_desired = eval(parser.get('homebase', 'kegs'))
+keg_opts = eval(parser.get('homebase', 'keg_opts'))
 
 kegs_to_install = [x for x in kegs_desired if x not in kegs_installed]
 
@@ -27,11 +30,17 @@ if len(kegs_to_install) == 0:
 	print "No kegs left to install"
 else:
 	for keg in kegs_to_install:
-		subprocess.call('brew install ' % keg, shell=True)
+		if keg in keg_opts:
+			subprocess.call('brew install ' + keg_opts[keg], shell=True)
+		else:
+			subprocess.call('brew install ' % keg, shell=True)
 
+
+# install pips
 pips_installed = subprocess.check_output('pip list', shell=True).split()
 
 pips_desired = eval(parser.get('homebase', 'pips'))
+pip_opts = eval(parser.get('homebase', 'pip_opts'))
 
 pips_to_install = [x for x in pips_desired if x not in pips_installed and not x.startswith('(')]
 
@@ -40,11 +49,13 @@ if len(pips_to_install) == 0:
 else:
 	print "pips: " + ", ".join(pips_to_install)
 	for pip in pips_to_install:
-		subprocess.call('pip install ' + pip, shell=True)
+		if pip in pip_opts:
+			subprocess.call('pip install ' + pip_opts[pip], shell=True)
+		else:
+			subprocess.call('pip install ' + pip, shell=True)
 
-# install git
+subprocess.call('dotfiles -C ~/homebase/dotfiles/dotfilesrc -s -f', shell=True)
 
-# test for git
 
 
 
--- homebase.sh
+++ homebase.sh
@@ -4,35 +4,4 @@ if [[ ! "$(type -P gcc)" && "$OSTYPE" =~ ^darwin ]]; then
   exit 1
 fi
 
-# If Git is not installed...
-if [[ ! "$(type -P git)" ]]; then
-  # OSX
-  if [[ "$OSTYPE" =~ ^darwin ]]; then
-    # It's easiest to get Git via Homebrew, so get that first.
-    if [[ ! "$(type -P brew)" ]]; then
-      e_header "Installing Homebrew"
-      true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-    fi
-    # If Homebrew was installed, install Git.
-    if [[ "$(type -P brew)" ]]; then
-      e_header "Updating Homebrew"
-      brew update
-      e_header "Installing Git"
-      brew install git
-    fi
-  # Ubuntu.
-  elif [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
-    # Git is fairly easy.
-    e_header "Installing Git"
-    sudo apt-get -qq install git-core
-  fi
-fi
-
-# If Git isn't installed by now, something exploded. We gots to quit!
-if [[ ! "$(type -P git)" ]]; then
-  e_error "Git should be installed. It isn't. Aborting."
-  exit 1
-fi
-
-# Update existing sudo time stamp if set, otherwise do nothing.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+python homebase.py
