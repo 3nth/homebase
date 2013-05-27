1b352bae0bb3770aac4da0e1a744917e479bf443
Derek Flenniken <EMAIL>
2013-05-27 09:05:03 -0700

    moved powerline config, fixed powerline path, made vimrc more responsive

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -1,4 +1,5 @@
 if [[ "$(type -P powerline)" ]]; then
-    SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-    . $SP/powerline/bindings/bash/powerline.sh
+    #SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+    #. $SP/powerline/bindings/bash/powerline.sh
+    . ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
 fi
