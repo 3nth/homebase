d53b4f5f2a60bc3d9fba6594c075c153d96b5311
Derek Flenniken <EMAIL>
2013-05-27 09:21:58 -0700

    figure out the powerline path

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -1,5 +1,6 @@
 if [[ "$(type -P powerline)" ]]; then
     #SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-    #. $SP/powerline/bindings/bash/powerline.sh
-    . ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
+    SP=`python -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
+    . $SP/bindings/bash/powerline.sh
+    #. ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
 fi
