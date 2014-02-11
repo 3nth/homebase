90556f846c15233aadbea091d35064aeff7a2e80
Derek Flenniken <derek@enth.net>
2014-02-10 22:10:37 -0800

    fixed

--- dotfiles/source/50_python.sh
+++ dotfiles/source/50_python.sh
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 #PYTHON_PATH=/usr/local/share/python
 #PATH=$PYTHON_PATH:$(path_remove $PYTHON_PATH)
 #export PATH
-source virtualenvwrapper.sh
-export PROJECT_HOME=~/projects
-=======
-# PYTHON_PATH=/usr/local/share/python
-# PATH=$PYTHON_PATH:$(path_remove $PYTHON_PATH)
-# export PATH
-
 export PROJECT_HOME=~/projects
 export WORKON_HOME=~/envs
-source /usr/local/bin/virtualenvwrapper.sh
+source virtualenvwrapper.sh
 
->>>>>>> 7698db069fd00230b204c382b52dc3b5e9eb6379
 # pip bash completion start
 _pip_completion()
 {
