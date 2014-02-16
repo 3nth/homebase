1e4ebd64a2df3b4e97c71c10133cc16d685de08d
Derek Flenniken <derek.flenniken@ucsf.edu>
2014-02-16 15:08:44 -0800

    use python2.7 specifically for powerline

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -1,6 +1,6 @@
 if [[ "$(type -P powerline)" ]]; then
     #SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-    SP=`python -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
+    SP=`python2.7 -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
     . $SP/bindings/bash/powerline.sh
     #. ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
 fi
