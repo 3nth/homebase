cbfda70cfc1630aa5a5b9ac9177ac06cf4847395
Derek Flenniken <derek@higgs.local>
2015-07-12 00:52:01 -0700

    powerline updated

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -1,5 +1,8 @@
 if [[ "$(type -P powerline)" ]]; then
     #SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
     SP=`python2.7 -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
     . $SP/bindings/bash/powerline.sh
     #. ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
