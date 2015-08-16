4991c6c7b33a5931a52d97a78207c04cfdee6e9f
Derek Flenniken <derek.flenniken@ucsf.edu>
2015-08-16 12:13:57 -0700

    cleanup powerline

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -1,9 +1,7 @@
-if [[ "$(type -P powerline)" ]]; then
-    #SP=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+if [[ "$(type -P powerline-daemon)" ]]; then
     powerline-daemon -q
     POWERLINE_BASH_CONTINUATION=1
     POWERLINE_BASH_SELECT=1
     SP=`python2.7 -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
     . $SP/bindings/bash/powerline.sh
-    #. ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
 fi
