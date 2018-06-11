e80e0f1078ca20279b53b67ea7116c28d7596bde
Derek Flenniken <Derek.Flenniken@ucsf.edu>
2018-06-11 11:53:00 -0700

    powerline for python3

--- dotfiles/source/60_powerline.sh
+++ dotfiles/source/60_powerline.sh
@@ -2,6 +2,5 @@ if [[ "$(type -P powerline-daemon)" ]]; then
     powerline-daemon -q
     POWERLINE_BASH_CONTINUATION=1
     POWERLINE_BASH_SELECT=1
-    SP=`python2.7 -c "from os import path; import powerline; print(path.dirname(powerline.__file__))"`
-    . $SP/bindings/bash/powerline.sh
+    . /usr/local/lib/python3.6/site-packages/powerline/bindings/bash/powerline.sh
 fi
